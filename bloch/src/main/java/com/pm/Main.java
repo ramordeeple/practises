09888b08c4f64805a7ca0b9e78588986733a70ff
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-08-21 05:24:30 +0300

    checking out objects

---
+++ bloch/src/main/java/com/pm/Main.java
@@ -0,0 +1,30 @@
+package com.pm;
+
+
+import com.pm.ch2.Calzone;
+import com.pm.ch2.NutritionFacts;
+import com.pm.ch2.NyPizza;
+import com.pm.ch2.ValueOfFrom;
+
+import static com.pm.ch2.NyPizza.Size.*;
+import static com.pm.ch2.Pizza.Topping.*;
+
+public class Main {
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts
+                .Builder(240, 8)
+                .calories(100)
+                .sodium(35)
+                .carbohydrate(27)
+                .build();
+
+        NyPizza pizza = new NyPizza.Builder(SMALL)
+                .addTopping(SAUSAGE)
+                .addTopping(ONION).build();
+
+        Calzone calzone = new Calzone.Builder()
+                .addTopping(HAM)
+                .addTopping(SAUSAGE).build();
+    }
+
+}
