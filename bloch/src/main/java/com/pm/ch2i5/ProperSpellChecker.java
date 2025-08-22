0d2740872d785d54f5952973467c67f6a5522984
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-08-22 23:33:45 +0300

    creating proper approach for a class with DI

---
+++ bloch/src/main/java/com/pm/ch2i5/ProperSpellChecker.java
@@ -0,0 +1,18 @@
+package com.pm.ch2i5;
+
+
+import java.util.List;
+import java.util.Objects;
+
+/** It provides flexibility & testability*/
+public class ProperSpellChecker {
+    private final Lexicon dictionary;
+
+    /** Gets the dictionary from outside */
+    public ProperSpellChecker(Lexicon dict) {
+        this.dictionary = Objects.requireNonNull(dict);
+    }
+
+    public boolean isValid(String word) {return true;}
+    public List<String> suggestions(String typo) {return null;}
+}
