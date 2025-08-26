0b36674941b328451756509962c971be6cfee8da
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-08-26 16:45:50 +0300

    elegant try-with-resources example

---
+++ bloch/src/main/java/com/pm/ch2i9/Elegant.java
@@ -0,0 +1,23 @@
+package com.pm.ch2i9;
+
+import java.io.*;
+
+public class Elegant {
+    static String firstLineOfFile(String path) throws IOException {
+        try (BufferedReader br = new BufferedReader(
+                new FileReader(path))) {
+            return br.readLine();
+        }
+    }
+
+    static void copy(String src, String dst) throws IOException {
+        try (InputStream in = new FileInputStream(src);
+             OutputStream out = new FileOutputStream(dst)) {
+
+            byte[] buf = new byte[1024];
+            int n;
+            while ((n = in.read(buf)) >= 0)
+                out.write(buf, 0, n);
+        }
+    }
+}
