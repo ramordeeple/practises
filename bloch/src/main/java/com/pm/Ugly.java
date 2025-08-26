9aa8e297d725f7d0c35ad5130761d62ea154dbb4
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-08-26 16:40:30 +0300

    ugly option of try-finally

---
+++ bloch/src/main/java/com/pm/Ugly.java
@@ -0,0 +1,24 @@
+package com.pm;
+
+import java.io.*;
+
+import static java.lang.System.in;
+
+public class Ugly {
+    static void copy(String src, String dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        try {
+            OutputStream out = new FileOutputStream(dst);
+            try {
+                byte[] buf = new byte[1024];
+                int n;
+                while ((n = in.read(buf)) >= 0)
+                    out.write(buf, 0, n);
+
+            finally {
+                out.close();
+            }}
+            finally {
+                in.close();
+            }
+        }
