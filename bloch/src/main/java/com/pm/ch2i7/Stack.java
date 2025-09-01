37fb527988e1f512b8a176a57eb6946593781502
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-09-01 16:53:59 +0300

    examples of Cloneable class and clone() methods

--- bloch/src/main/java/com/pm/ch2i7/Stack.java
+++ bloch/src/main/java/com/pm/ch2i7/Stack.java
@@ -29,4 +29,62 @@ public class Stack {
         if (elements.length == size)
             elements = Arrays.copyOf(elements, 2 * size + 1);
     }
+
+    @Override public Stack clone() {
+        try {
+            Stack result = (Stack) super.clone();
+            result.elements = elements.clone();
+
+            return result;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
+
+class HashTable implements Cloneable {
+    private Entry[] buckets;
+
+    private static class Entry {
+        final Object key;
+        Object value;
+        Entry next;
+
+        Entry(Object key, Object value, Entry next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        /** Recursively copy the linked list headed by this Entry */
+        Entry deepCopy() {
+            return new Entry(key, value, next == null ? null : next.deepCopy());
+        }
+    }
+
+    @Override public HashTable clone() {
+        try {
+            HashTable result = (HashTable) super.clone();
+            result.buckets = new Entry[buckets.length];
+
+            for (int i = 0; i < buckets.length; i++)
+                if (buckets[i] != null)
+                    result.buckets[i] = buckets[i].deepCopy();
+
+            return result;
+        }
+        catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+    Entry deepCopy() {
+        Entry result = new Entry(key, value, next);
+
+        for (Entry p = result; p.next != null; p = p.next)
+            p.next = new Entry(p.next.key, p.next.value, p.next.next);
+
+        return result;
+    }
+
 }
