07532026f06ca9ef8d2ff22f4c1ab5e784a72c93
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-09-11 07:42:35 +0300

    adding 2 methods in Operation

--- chapter6/src/main/java/com/pm/i34/Operation.java
+++ chapter6/src/main/java/com/pm/i34/Operation.java
@@ -1,10 +1,36 @@
 package com.pm.i34;
 
-public enum Operation {
-    PLUS {public double apply(double a, double b) {return a + b;}},
-    MINUS {public double apply(double a, double b) {return a - b;}},
-    MULTIPLY {public double apply(double a, double b) {return a * b;}},
-    DIVIDE {public double apply(double a, double b) {return a / b;}};
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toMap;
+
+public enum Operation {
+    PLUS ("+") {public double apply(double a, double b) {return a + b;}},
+    MINUS ("-") {public double apply(double a, double b) {return a - b;}},
+    MULTIPLY ("*") {public double apply(double a, double b) {return a * b;}},
+    DIVIDE ("/") {public double apply(double a, double b) {return a / b;}};
+
+    private final String symbol;
+
+    Operation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public String toString() {
+        return "Operation{" +
+                "symbol='" + symbol + '\'' +
+                '}';
+    }
+
+    private static final Map<String, Operation> stringToOperation =
+            Stream.of(values()).collect(
+                    toMap(Object::toString, e -> e));
+
+    public static Optional<Operation> fromString(String symbol) {
+        return Optional.ofNullable(stringToOperation.get(symbol));
+    }
     public abstract double apply(double a, double b);
 }
