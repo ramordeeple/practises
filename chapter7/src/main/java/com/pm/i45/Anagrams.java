b83f0b857168afc1da4cd8233e9aa2a0f5d4c810
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-09-13 09:15:31 +0300

    example of boilerplate program with using of stream api

--- chapter7/src/main/java/com/pm/i45/Anagrams.java
+++ chapter7/src/main/java/com/pm/i45/Anagrams.java
@@ -1,30 +1,27 @@
 package com.pm.i45;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class Anagrams {
-    public static void main(String[] args) {
-        File dictionary = new File(args[0]);
+    public static void main(String[] args) throws IOException {
+        Path dictionary = Paths.get(args[0]);
         int minGroupSize = Integer.parseInt(args[1]);
 
-        Map<String, Set<String>> groups = new HashMap<>();
-        try (Scanner sc = new Scanner(dictionary)) {
-            while (sc.hasNextLine()) {
-                String word = sc.next();
-                groups.computeIfAbsent(alphabetize(word),
-                        (unused) -> new TreeSet<>())
-                        .add(word);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        try (Stream<String> words = Files.lines(dictionary)) {
+            words.collect(groupingBy(word -> alphabetize(word)))
+                    .values().stream()
+                    .filter(group -> group.size() >= minGroupSize)
+                    .forEach(g -> System.out.println(g.size() + ": " + g));
         }
-
-        for (Set<String> group : groups.values())
-            if (group.size() >= minGroupSize)
-                System.out.println(group.size() + ": " + group);
     }
-
     private static String alphabetize(String s) {
         char[] a = s.toCharArray();
         Arrays.sort(a);
