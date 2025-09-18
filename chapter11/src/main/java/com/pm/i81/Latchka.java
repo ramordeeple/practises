c695f9fb11dc49a7ce7f7c82bfc6f16fd7a0b110
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-09-18 19:22:16 +0300

    example of countdown latch

---
+++ chapter11/src/main/java/com/pm/i81/Latchka.java
@@ -0,0 +1,33 @@
+package com.pm.i81;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+
+public class Latchka {
+    public static long time(Executor executor, int concurrency,
+                            Runnable action) throws InterruptedException {
+        CountDownLatch ready = new CountDownLatch(concurrency);
+        CountDownLatch start = new CountDownLatch(1);
+        CountDownLatch done = new CountDownLatch(concurrency);
+
+        for (int i = 0; i < concurrency; i++) {
+            executor.execute(() -> {
+                ready.countDown();
+                try {
+                    start.await();
+                    action.run();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    done.countDown();
+                }
+            });
+        };
+
+        ready.await();
+        long startNanos = System.nanoTime();
+        done.countDown();
+
+        return System.nanoTime() - startNanos;
+    }
+}
